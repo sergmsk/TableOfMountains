7dda921a77aeec4e2c657b6caf6a34431ab85415
KS <me@gmail.com>
2017-03-24 12:21:06 +0300

    код для вывода таблицы

--- tableOfMountains/app.js
+++ tableOfMountains/app.js
@@ -16,6 +16,28 @@ function colWidths(rows) {
     });
 }
 
+function drawTable(rows) {
+    var heights = rowHeights(rows);
+    var widths = colWidths(rows);
+
+    function drawLine(blocks, lineNo) {
+        return blocks.map(function (block) {
+            return block[lineNo];
+        }).join(" ");
+    }
+
+    function drawRow(row, rowNum) {
+        var blocks = row.map(function (cell, colNum) {
+            return cell.draw(widths[colNum], heights[rowNum]);
+        });
+        return blocks[0].map(function (_, lineNo) {
+            return drawLine(blocks, lineNo);
+        }).join("\n");
+    }
+
+    return rows.map(drawRow).join("\n");
+}
+
 function dataTable(data) {
     var keys = Object.keys(data[0]);
     var headers = keys.map(function (name) {
