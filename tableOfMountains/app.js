b2eb4b959ca103ef8bfe47aeb0607b4870facb6d
KS <me@gmail.com>
2017-03-24 12:43:23 +0300

    функция UnderlinedCell выделяет верхнюю строчку

--- tableOfMountains/app.js
+++ tableOfMountains/app.js
@@ -78,4 +78,18 @@ function dataTable(data) {
     return [headers].concat(body);
 }
 
+function UnderlinedCell(inner) {
+    this.inner = inner;
+};
+UnderlinedCell.prototype.minWidth = function () {
+    return this.inner.minWidth();
+};
+UnderlinedCell.prototype.minHeight = function () {
+    return this.inner.minHeight() + 1;
+};
+UnderlinedCell.prototype.draw = function (width, height) {
+    return this.inner.draw(width, height - 1)
+        .concat([repeat("-", width)]);
+};
+
 console.log(drawTable(dataTable(MOUNTAINS)));
