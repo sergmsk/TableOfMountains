e4348bbf92ea0dd750940fa03b929b8c98331028
KS <me@gmail.com>
2017-03-24 12:10:27 +0300

    добавляем функцию, строящую сетку ячеек из нашего набора данных

--- tableOfMountains/app.js
+++ tableOfMountains/app.js
@@ -1,3 +1,16 @@
 'use strict';
 
+function dataTable(data) {
+    var keys = Object.keys(data[0]);
+    var headers = keys.map(function (name) {
+        return new UnderlinedCell(new TextCell(name));
+    });
+    var body = data.map(function (row) {
+        return keys.map(function (name) {
+            return new TextCell(String(row[name]));
+        });
+    });
+    return [headers].concat(body);
+}
+
 console.log(drawTable(dataTable(MOUNTAINS)));
